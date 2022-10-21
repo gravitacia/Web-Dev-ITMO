ee2b0f3c631ca4a6c16a21529434513410fb172f
Aleksander Kosarskii <kocagg@mail.ru>
2022-10-21 11:54:28 +0500

    refactor : change navigation bar and add navigation for small screens

---
+++ index.js
@@ -0,0 +1,10 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
