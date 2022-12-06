26c7853b27e3eb81702d24f80581e242e19038a7
gravitacia <kocagg@mail.ru>
2022-12-06 03:17:23 +0300

    feat : add infinity scroll features

--- addpost.js
+++ addpost.js
@@ -1,30 +1,37 @@
-const navBar = document.querySelector(".topnav")
-const addPostElement = document.querySelector(".add-post")
+function addPost(){
+    const navBar = document.getElementById("topnav")
+    const addPostElement = document.getElementById("addPost")
 
-addPostElement.addEventListener('click', () => {
+    addPostElement.addEventListener('click', () => {
 
-    const post = document.createElement("div");
-    post.className = "subforum";
-    post.innerHTML = "<div class=\"subforum-title\">\n" +
-        "                    <h1>General Information</h1>\n" +
-        "                </div>\n" +
-        "                <div class=\"subforum-row center\">\n" +
-        "                    <div class=\"subforum-stats subforum-column center\">\n" +
-        "                    <div class=\"vote-button\">\n" +
-        "                        <div class=\"like\">\n" +
-        "                            <span class=\"plus cursor\">+</span> <span class=\"counter\">0</span> <span class=\"minus cursor\">-</span>\n" +
-        "                        </div>\n" +
-        "                    </div>\n" +
-        "                    </div>\n" +
-        "                    <div class=\"subforum-description subforum-column center\">\n" +
-        "                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum dolore hic\n" +
-        "                            necessitatibus nostrum optio possimus sequi veniam. Accusantium amet deserunt dolorum id\n" +
-        "                            illo incidunt rem rerum soluta tempore voluptas!</p>\n" +
-        "                    </div>\n" +
-        "                    <div class=\"subforum-stats subforum-column center\">\n" +
-        "                        <span>Author | Date</span>\n" +
-        "                    </div>\n" +
-        "                </div>";
+        const post = document.createElement("div");
+        post.className = "subforum";
+        post.innerHTML = "<div class=\"subforum-title\">\n" +
+            "                    <h1>General Information</h1>\n" +
+            "                </div>\n" +
+            "                <div class=\"subforum-row center\">\n" +
+            "                    <div class=\"subforum-stats subforum-column center\">\n" +
+            "                    <div class=\"vote-button\">\n" +
+            "                        <div class=\"like\">\n" +
+            "                            <span class=\"plus cursor\">+</span> <span class=\"counter\">0</span> <span class=\"minus cursor\">-</span>\n" +
+            "                        </div>\n" +
+            "                    </div>\n" +
+            "                    </div>\n" +
+            "                    <div class=\"subforum-description subforum-column center\">\n" +
+            "                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum dolore hic\n" +
+            "                            necessitatibus nostrum optio possimus sequi veniam. Accusantium amet deserunt dolorum id\n" +
+            "                            illo incidunt rem rerum soluta tempore voluptas!</p>\n" +
+            "                    </div>\n" +
+            "                    <div class=\"subforum-stats subforum-column center\">\n" +
+            "                        <span>Author | Date</span>\n" +
+            "                    </div>\n" +
+            "                </div>";
 
-    navBar.after(div);
-})
+        navBar.after(post);
+    })
+
+    post.innerHTML;
+}
+
+
+
--- index.js
+++ index.js
@@ -21,7 +21,38 @@ document.addEventListener("click", function (event) {
 
 const container = document.getElementById("container");
 const postIncrease = 3;
-const cardLimit = 99;
+const postLimit = 99;
 const pageCount = Math.ceil(cardLimit / postIncrease);
 let currentPage = 1;
 
+const createPost = () => {
+    const post = document.createElement("div");
+    post.className = "subforum";
+    post.innerHTML = addPost();
+    container.appendChild(post);
+}
+
+const showPost = (pageIndex) => {
+    currentPage = pageIndex;
+
+    const startRange = (pageIndex - 1) * cardIncrease;
+    const endRange = currentPage === pageCount ? postLimit : pageIndex * cardIncrease;
+
+    for (let i = startRange + 1; i <= endRange; i++) {
+        createPost();
+    }
+}
+
+window.onload = function () {
+    showPost(currentPage);
+};
+
+const handleInfiniteScroll = () => {
+    const endOfPage = window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+
+    if (endOfPage) {
+        showPost(currentPage + 1);
+    }
+};
+
+window.addEventListener("scroll", handleInfiniteScroll);
