9b341da0e0a4859262597899d26dd020d6fa93d8
gravitacia <kocagg@mail.ru>
2022-12-16 14:03:27 +0300

    feat : add post rendering from localstorage

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function navButton() {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("plus")) {
+        console.log(event.target.parentElement.querySelector(".counter").innerHTML)
+        event.target.parentElement.querySelector(".counter").innerText = parseInt(event.target.parentElement.querySelector(".counter").innerText) + 1;
+    }
+
+    if (event.target.classList.contains("minus")) {
+        console.log(event.target.parentElement.querySelector(".counter").innerHTML)
+        event.target.parentElement.querySelector(".counter").innerText = parseInt(event.target.parentElement.querySelector(".counter").innerText) - 1;
+    }
+});
---
+++ js/showpost.js
@@ -0,0 +1,46 @@
+let navBar = document.getElementById("container");
+
+let posts
+
+if (!localStorage.getItem("posts")) {
+    posts = []
+}
+else {
+    posts = JSON.parse(localStorage.getItem("posts"))
+}
+
+for (let i = 0; i < posts.length; i++) {
+    console.log(posts[i])
+    let count = posts[i].rating
+    let authorName = posts[i].author
+    let title = posts[i].title
+    let id = posts[i].id
+    let text = posts[i].subject
+
+    let div = document.createElement('div');
+    div.className = "post";
+    div.innerHTML = `<div class="container">
+            <div class="subforum">
+                <div class="subforum-title">
+                    <h1>${title}</h1>
+                </div>
+
+                <div class="subforum-row center">
+                    <div class="subforum-stats subforum-column center">
+                    <div class="vote-button">
+                        <div class="like">
+                            <span class="plus cursor">+</span> <span id="${id}" class="counter">${count}</span> <span class="minus cursor">-</span>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="subforum-description subforum-column center">
+                        <p>${text}</p>
+                    </div>
+                    <div class="subforum-stats subforum-column center">
+                        <span>${authorName} | Date</span>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    document.body.appendChild(div);
+}
