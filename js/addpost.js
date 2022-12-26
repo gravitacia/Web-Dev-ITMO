136f45c65db7ed806417f139508c38a17335db30
Aleksander Kosarskii <kocagg@mail.ru>
2022-12-26 20:50:26 +0500

    feat : add reddit api for post rendering

--- js/addpost.js
+++ js/addpost.js
@@ -1,6 +1,11 @@
+import { format } from 'https://unpkg.com/browse/fecha@4.2.3/';
+
+format(new Date(), 'YYYY-MM-DD HH:mm:ss');
+
 const formElement = document.getElementById('post-form')
 
 let idNum = +localStorage.getItem("idNum") || 2
+let userID = 100;
 
 formElement.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -9,6 +14,7 @@ formElement.addEventListener('submit', (e) => {
     let subject = formData.get('subject');
     let title = formData.get('title');
     let date = new Date();
+    let dayWrapper = moment(date);
 
     idNum = (idNum + 1)
     localStorage.setItem("idNum", idNum)
@@ -21,7 +27,8 @@ formElement.addEventListener('submit', (e) => {
         rating: 0,
         day: date.getDate(),
         month: date.getMonth(),
-        year: date.getFullYear()
+        year: date.getFullYear(),
+        postDate: dayWrapper
     }
 
     let posts
@@ -36,5 +43,8 @@ formElement.addEventListener('submit', (e) => {
     posts.push(object)
 
     localStorage.setItem("posts", JSON.stringify(posts))
+
+
+
     window.location.reload();
 })
