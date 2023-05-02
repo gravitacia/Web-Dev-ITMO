4b4ff49a60222dba4c440afd6b84facaa8beb9d7
gravitacia <kocagg@mail.ru>
2023-05-02 17:41:22 +0300

    feat : add search

--- js/index.js
+++ js/index.js
@@ -65,21 +65,39 @@ function renderPosts(item) {
 }
 
 
-const postList = document.getElementById('post-list');
-let posts = JSON.parse(localStorage.getItem('posts')) || [];
+const posts = JSON.parse(localStorage.getItem('posts'));
+const container = document.getElementById('posts');
+posts.forEach(elem => {
+    const post = document.createElement('div');
+    post.innerHTML = `
+    <div class="container">
+      <div class="subforum">
+          <div class="subforum-title">
+              <h1>${elem.data.title}</h1>
+          </div>
 
-// Создаем элементы для каждого поста из localStorage и добавляем их на страницу
-posts.forEach(post => {
-    const postItem = document.createElement('li');
-    const postTitle = document.createElement('h3');
-    postTitle.textContent = post.title;
-    const postDate = document.createElement('p');
-    postDate.textContent = post.date;
-    const postSubject = document.createElement('p');
-    postSubject.textContent = post.subject;
-    postItem.appendChild(postTitle);
-    postItem.appendChild(postDate);
-    postItem.appendChild(postSubject);
-    postList.appendChild(postItem);
+          <div class="subforum-row center">
+              <div class="subforum-stats subforum-column center">
+                  <div class="vote-button">
+                      <div class="like">
+                          <span class="plus cursor">+</span> <span id="${elem.data.id}" class="counter">0</span> <span class="minus cursor">-</span>
+                      </div>
+                  </div>
+              </div>
+              <div class="subforum-description subforum-column center">
+                  <p>${elem.data.selftext}</p>
+              </div>
+              <div class="subforum-stats subforum-column center">
+                  <span>
+                      <p>${elem.data.author}</p> 
+                      <p>${new Date(elem.data.created_utc * 1000).toLocaleDateString()}</p>
+                  </span>
+              </div>
+          </div>
+      </div>
+    </div>
+  `;
+    container.appendChild(post);
 });
+
 
