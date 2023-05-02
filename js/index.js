33c5a97af54db7f2bd1f22c1d84bfc9e06512580
gravitacia <kocagg@mail.ru>
2023-05-02 17:49:11 +0300

    feat : add search

--- js/index.js
+++ js/index.js
@@ -65,39 +65,52 @@ function renderPosts(item) {
 }
 
 
-const posts = JSON.parse(localStorage.getItem('posts'));
-const container = document.getElementById('posts');
-posts.forEach(elem => {
-    const post = document.createElement('div');
-    post.innerHTML = `
-    <div class="container">
-      <div class="subforum">
-          <div class="subforum-title">
-              <h1>${elem.data.title}</h1>
-          </div>
+let posts
 
-          <div class="subforum-row center">
-              <div class="subforum-stats subforum-column center">
-                  <div class="vote-button">
-                      <div class="like">
-                          <span class="plus cursor">+</span> <span id="${elem.data.id}" class="counter">0</span> <span class="minus cursor">-</span>
-                      </div>
-                  </div>
-              </div>
-              <div class="subforum-description subforum-column center">
-                  <p>${elem.data.selftext}</p>
-              </div>
-              <div class="subforum-stats subforum-column center">
-                  <span>
-                      <p>${elem.data.author}</p> 
-                      <p>${new Date(elem.data.created_utc * 1000).toLocaleDateString()}</p>
-                  </span>
-              </div>
-          </div>
-      </div>
-    </div>
-  `;
-    container.appendChild(post);
-});
+if (!localStorage.getItem("posts")) {
+    posts = []
+}
+else {
+    posts = JSON.parse(localStorage.getItem("posts"))
+}
+
+for (let i = 0; i < posts.length; i++) {
+    console.log(posts[i])
+    let count = posts[i].rating
+    let authorName = posts[i].author
+    let title = posts[i].title
+    let id = posts[i].id
+    let text = posts[i].subject
+    let day = posts[i].day
+    let month = posts[i].month
+    let year = posts[i].year
+
+    let div = document.createElement('div');
+    div.className = "post";
+    div.innerHTML = `<div class="container">
+            <div class="subforum">
+                <div class="subforum-title">
+                    <h1>${title}</h1>
+                </div>
+
+                <div class="subforum-row center">
+                    <div class="subforum-stats subforum-column center">
+                    <div class="vote-button">
+                        <div class="like">
+                            <span class="plus cursor">+</span> <span id="${id}" class="counter">${count}</span> <span class="minus cursor">-</span>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="subforum-description subforum-column center">
+                        <p>${text}</p>
+                    </div>
+                    <div class="subforum-stats subforum-column center">
+                        <span>${authorName} | ${day}.${month}.${year}</span>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    document.body.appendChild(div);
+}
 
 
