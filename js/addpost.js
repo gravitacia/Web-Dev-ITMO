3781f8411ab1a0a2d2e9aaea7be6ffc06ab84bce
gravitacia <kocagg@mail.ru>
2023-05-02 17:00:13 +0300

    feat : add search

--- js/addpost.js
+++ js/addpost.js
@@ -1,46 +1,13 @@
-import  ms from 'https://unpkg.com/ms@2.1.3/index.js';
-
-
-const formElement = document.getElementById('post-form')
-
-let idNum = +localStorage.getItem("idNum") || 2
-
-formElement.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(formElement);
-    let name = formData.get('username');
-    let subject = formData.get('subject');
-    let title = formData.get('title');
-    let date = new Date();
-
-    idNum = (idNum + 1)
-    localStorage.setItem("idNum", idNum)
-
-    let object = {
-        id: idNum,
-        author: name,
-        title: title,
-        subject: subject,
-        rating: 0,
-        day: date.getDate(),
-        month: date.getMonth(),
-        year: date.getFullYear()
-    }
-
-    let posts
-
-    if (!localStorage.getItem("posts")) {
-        posts = []
-    }
-    else {
-        posts = JSON.parse(localStorage.getItem("posts"))
-    }
-
-    posts.push(object)
-
-    localStorage.setItem("posts", JSON.stringify(posts))
-
-
-
-    window.location.reload();
-})
+const form = document.getElementById('post-form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const title = document.getElementById('title').value;
+    const subject = document.getElementById('subject').value;
+    const date = luxon.DateTime.now().toString();
+    const post = { username, title, subject, date };
+    let posts = JSON.parse(localStorage.getItem('posts')) || [];
+    posts.push(post);
+    localStorage.setItem('posts', JSON.stringify(posts));
+    alert('Post added successfully');
+});
--- js/showpost.js
+++ js/showpost.js
@@ -1,4 +1,4 @@
-import  format  from 'https://unpkg.com/fecha@4.2.3/lib/fecha.js';
+import { format }  from 'https://unpkg.com/fecha@4.2.3/lib/fecha.js';
 
 
 
@@ -52,44 +52,88 @@ for (let i = 0; i < posts.length; i++) {
 }
 
 fetch('https://www.reddit.com/r/Jokes.json')
-    .then((response) => response.json())
-        .then((data) => {
-            renderPosts(data.data.children.map((data) => data.data))
-        })
+    .then(response => response.json())
+    .then(data => {
+        const posts = data.data.children;
+        const container = document.getElementById('posts');
+        posts.forEach(elem => {
+            const post = document.createElement('div');
+            post.innerHTML = `
+        <div class="container">
+          <div class="subforum">
+              <div class="subforum-title">
+                  <h1>${elem.data.title}</h1>
+              </div>
+
+              <div class="subforum-row center">
+                  <div class="subforum-stats subforum-column center">
+                      <div class="vote-button">
+                          <div class="like">
+                              <span class="plus cursor">+</span> <span id="${elem.data.id}" class="counter">0</span> <span class="minus cursor">-</span>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="subforum-description subforum-column center">
+                      <p>${elem.data.selftext}</p>
+                  </div>
+                  <div class="subforum-stats subforum-column center">
+                      <span>
+                          <p>${elem.data.author}</p> 
+                          <p>${new Date(elem.data.created_utc * 1000).toLocaleDateString()}</p>
+                      </span>
+                  </div>
+              </div>
+          </div>
+        </div>
+      `;
+            container.appendChild(post);
+        });
+    })
+    .catch(error => console.log(error));
 
 
-
-function renderPosts(item) {
-    item.forEach((elem) => {
-        let date = new Date(elem.created_utc)
-        let div = document.createElement('div');
-        div.className = "post";
-        div.innerHTML = `<div class="container">
+const form = document.getElementById('search-form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const subreddit = document.getElementById('search-input').value;
+    fetch(`https://www.reddit.com/r/${subreddit}.json`)
+        .then(response => response.json())
+        .then(data => {
+            const posts = data.data.children;
+            const container = document.getElementById('posts');
+            container.innerHTML = '';
+            posts.forEach(elem => {
+                const post = document.createElement('div');
+                post.innerHTML = `
+          <div class="container">
             <div class="subforum">
                 <div class="subforum-title">
-                    <h1>${elem.title}</h1>
+                    <h1>${elem.data.title}</h1>
                 </div>
 
                 <div class="subforum-row center">
                     <div class="subforum-stats subforum-column center">
-                    <div class="vote-button">
-                        <div class="like">
-                            <span class="plus cursor">+</span> <span id="${elem.id}" class="counter">0</span> <span class="minus cursor">-</span>
+                        <div class="vote-button">
+                            <div class="like">
+                                <span class="plus cursor">+</span> <span id="${elem.data.id}" class="counter">0</span> <span class="minus cursor">-</span>
+                            </div>
                         </div>
                     </div>
-                    </div>
                     <div class="subforum-description subforum-column center">
-                        <p>${elem.selftext}</p>
+                        <p>${elem.data.selftext}</p>
                     </div>
                     <div class="subforum-stats subforum-column center">
                         <span>
-                        <p>${elem.author}</p> 
-                        <p>${format(date, 'mediumDate')}</p>
+                            <p>${elem.data.author}</p> 
+                            <p>${new Date(elem.data.created_utc * 1000).toLocaleDateString()}</p>
                         </span>
                     </div>
                 </div>
             </div>
-        </div>`;
-        document.body.appendChild(div);
-    })
-}
+          </div>
+        `;
+                container.appendChild(post);
+            });
+        })
+        .catch(error => console.log(error));
+});
